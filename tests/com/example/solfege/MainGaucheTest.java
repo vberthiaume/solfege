d25ef6ff8186cf88c9d954b8dd0dd865724d39bb
laloutre12 <sebast.bel@gmail.com>
2013-10-18 16:40:52 -0400

    repertoire change

---
+++ tests/com/example/solfege/MainGaucheTest.java
@@ -0,0 +1,51 @@
+package com.example.solfege;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainGaucheTest {
+
+    @Test
+    public void testMainGauche() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testMain() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testTest() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGenereAccordMidi() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testGenereAccordAbc() {
+        MainGauche tester = new MainGauche();
+        
+        System.out.println(tester.genereAccordAbc("maj",1));
+    }
+
+    @Test
+    public void testGauche() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSetaccord() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testDegre2midi() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
