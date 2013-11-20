4841c69beb140fd08a3acb755af2e0c207981ca9
laloutre12 <sebast.bel@gmail.com>
2013-11-20 15:13:26 -0500

    commit before push

--- gen/com/example/solfege/R.java
+++ gen/com/example/solfege/R.java
@@ -8,6 +8,14 @@
 package com.example.solfege;
 
 public final class R {
+    public static final class array {
+        public static final int inchannels_labels=0x7f070002;
+        public static final int inchannels_values=0x7f070003;
+        public static final int outchannels_labels=0x7f070004;
+        public static final int outchannels_values=0x7f070005;
+        public static final int srate_labels=0x7f070000;
+        public static final int srate_values=0x7f070001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,33 +25,63 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f090000;
+        public static final int activity_vertical_margin=0x7f090001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int din5=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090000;
-        public static final int action_settings=0x7f090002;
-        public static final int webView1=0x7f090001;
+        public static final int RelativeLayout1=0x7f0b0000;
+        public static final int action_settings=0x7f0b0004;
+        public static final int paired_devices=0x7f0b0003;
+        public static final int title_paired_devices=0x7f0b0002;
+        public static final int webView1=0x7f0b0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int device_list=0x7f030001;
+        public static final int device_name=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f0a0000;
     }
     public static final class raw {
-        public static final int piano_c1=0x7f040000;
+        public static final int extra_abs=0x7f050000;
+        public static final int piano_c1=0x7f050001;
+        public static final int silence=0x7f050002;
     }
     public static final class string {
-        public static final int Staff_desc=0x7f060004;
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int button_send=0x7f060001;
-        public static final int remplirBouton1=0x7f060005;
-        public static final int title_activity_main=0x7f060003;
+        public static final int Staff_desc=0x7f06001a;
+        public static final int action_settings=0x7f060018;
+        public static final int app_name=0x7f060016;
+        public static final int button_send=0x7f060017;
+        public static final int none_paired=0x7f060001;
+        public static final int pref_key_inchannels=0x7f060010;
+        public static final int pref_key_outchannels=0x7f060013;
+        public static final int pref_key_srate=0x7f06000d;
+        public static final int pref_sum=0x7f06000c;
+        public static final int pref_sum_inchannels=0x7f060012;
+        public static final int pref_sum_outchannels=0x7f060015;
+        public static final int pref_sum_srate=0x7f06000f;
+        public static final int pref_title=0x7f06000b;
+        public static final int pref_title_inchannels=0x7f060011;
+        public static final int pref_title_outchannels=0x7f060014;
+        public static final int pref_title_srate=0x7f06000e;
+        public static final int remplirBouton1=0x7f06001b;
+        public static final int select_device=0x7f060000;
+        public static final int title_activity_main=0x7f060019;
+        public static final int title_no_paired_devices=0x7f060003;
+        public static final int title_no_usb_devices_available=0x7f060008;
+        public static final int title_no_usb_midi_input_available=0x7f060009;
+        public static final int title_no_usb_midi_output_available=0x7f06000a;
+        public static final int title_paired_devices=0x7f060002;
+        public static final int title_select_bluetooth_device=0x7f060004;
+        public static final int title_select_usb_midi_device=0x7f060005;
+        public static final int title_select_usb_midi_input=0x7f060006;
+        public static final int title_select_usb_midi_output=0x7f060007;
     }
     public static final class style {
         /** 
@@ -67,10 +105,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080001;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080002;
+        public static final int DisableSoundEffects=0x7f080000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
